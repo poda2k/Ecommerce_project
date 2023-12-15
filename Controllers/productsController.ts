8734892125a5ac06b3aac88d95fa7b8287cf0112
poda2k <podayasser101@gmail.com>
2023-12-15 16:47:38 +0200

    product and category CRUD

--- Controllers/productsController.ts
+++ Controllers/productsController.ts
@@ -16,6 +16,23 @@ class productHandler {
 
     }
 
+    static async getSingleProduct(req : Request, res : Response) : Promise<void>{
+        const prodId = req.params.id ;
+
+        try{
+            const getOneProduct = await Products.findOne({
+                where:{
+                    id : prodId
+                }
+            })
+
+            res.json({massage : "product found" , products : getOneProduct}) ;
+        }catch(e){
+            console.log("error in Single Product");
+            throw e ;
+        }
+    }
+
     static async createProduct(req : Request, res : Response) : Promise<void>{
 
         const {prodName , description , imageUrl , price , quantity , color , categoryName} = req.body ;
@@ -28,16 +45,24 @@ class productHandler {
         if(getCategory == null){
             res.json({massage : "no categories found"}) ;
         }else{
-            const newProduct = await Products.create({
-                cat_id : getCategory.id,
-                name : prodName,
-                description : description,
-                price : price ,
-                quantity : quantity ,
-                imageUrl : imageUrl,
-                color : color
-            });
-            res.json({ massage : "product created successful" ,product : newProduct})
+            try{
+                const newProduct = await Products.create({
+                    cat_id : getCategory.id,
+                    name : prodName,
+                    description : description,
+                    price : price ,
+                    quantity : quantity ,
+                    imageUrl : imageUrl,
+                    color : color
+                });
+
+                res.json({ massage : "product created successful" ,product : newProduct})
+            }catch(e){
+                console.log("error creating product"); 
+                throw e ;
+            }
+
+            
         }
 
 
@@ -45,15 +70,74 @@ class productHandler {
 
     static async createCategory(req: Request, res: Response) : Promise<void>{
         const categoryName = req.body.categoryName ;
+        try{
+            const newCategory = await Categories.create({
+                categoryName : categoryName
+            })
 
-        const newCategory = await Categories.create({
-            categoryName : categoryName
-        })
+            res.json({massage : "category created" , category : newCategory}) ;
+        }catch(e){
+            console.log("error creating category")
+            throw e ;
+        }
+        
 
-        res.json({massage : "category created" , category : newCategory}) ;
+        
 
     }
 
+    static async updateProduct(req: Request, res: Response) : Promise<void>{
+        const prodId = req.params.id ;
+        const {prodName , description , imageUrl , price , quantity , color , categoryName} = req.body ;
+
+        // getting the category info //
+        const category = await Categories.findOne({
+            where:{
+                categoryName : categoryName
+            }
+        });
+        if(categoryName == null) {
+            res.json({massage : "category not found"}) ;
+        }else if(categoryName!=null){
+            try{
+                const updated = await Products.update({
+                    cat_id : category!.id,
+                    name : prodName,
+                    description : description,
+                    price : price ,
+                    quantity : quantity,
+                    imageUrl : imageUrl ,
+                    color : color
+                },{
+                    where : {
+                        id : prodId
+                    }
+                })
+                res.json({massage : "product updated" , product : updated}) ;
+            }catch(e){
+                console.log("error in update product") ;
+                throw e ;
+            }
+           
+        }
+    }
+
+    static async deleteProduct(res: Response, req : Request) : Promise<void>{
+        const prodId = req.params.id ;
+
+        try{
+            const deletedProduct = await Products.destroy({
+                where:{
+                    id : prodId
+                }
+            })
+
+            res.json({massage : "product deleted" , product : deletedProduct}) ;
+        }catch(e){
+            console.log("error in delete product")
+            throw e ;
+        }
+    }
 }
 
 export default productHandler ;
--- Routes/productsRoutes.ts
+++ Routes/productsRoutes.ts
@@ -3,12 +3,16 @@ import productHandler from "../Controllers/productsController";
 
 const router = Router();
 
+//=========================================================================================//
+
 //GET//
 
 router.get('/allProducts' , productHandler.getAllProducts) ;
+router.get('/singleProduct/:id', productHandler.getSingleProduct) ;
 
 //GET//
 
+//=========================================================================================//
 
 //POST//
 
@@ -16,6 +20,22 @@ router.post('/createProducts', productHandler.createProduct) ;
 router.post('/createCategory' , productHandler.createCategory) ;
 
 //POST//
+
+//=========================================================================================//
+
+//PUT//
+
+router.put('/updateProducts/:id', productHandler.updateProduct);
+
+//PUT//
+
+//=========================================================================================//
+
+//DELETE//
+
+router.delete('/deleteProduct/:id', productHandler.deleteProduct);
+
+//DELETE//
 
 
 
