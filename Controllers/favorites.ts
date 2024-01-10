06de76d1aa4eb2a6fe75efdc766f199d510bdb50
poda2k <podayasser101@gmail.com>
2024-01-10 20:51:28 +0200

    added favorites model

--- Controllers/favorites.ts
+++ Controllers/favorites.ts
@@ -5,11 +5,12 @@ class favoritesClass {
 
     static async postFavorites(req:Request, res:Response): Promise<void> {
 
-        const userId = req.params.userId ;
+        // userId extracted from the token which we still waiting for el bashmohands omar 3shan y7otha ....
+        //const userId = req.userId ;
         const prodId = req.params.prodId ;
         try{
             const newFavorite = await favorites.create({
-                user_id : userId ,
+              //  user_id : userId ,
                 product_id : prodId ,
                 is_purchased : false
             });
